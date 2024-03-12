1c559ab3a13bbdf60c990a2d35967632de4a1491
bardiafa <143993892+barry-far@users.noreply.github.com>
2024-03-12 22:50:03 +0330

    Update app.py

--- Files/app.py
+++ Files/app.py
@@ -11,19 +11,6 @@ fixed_text = """#profile-title: base64:8J+GkyBHaXRodWIgfCBCYXJyeS1mYXIg8J+ltw==
 #profile-web-page-url: https://github.com/barry-far/V2ray-Configs
 
 """
-import pybase64
-import base64 
-import requests
-import binascii
-import os
-
-fixed_text = """#profile-title: base64:8J+GkyBHaXRodWIgfCBCYXJyeS1mYXIg8J+ltw==
-#profile-update-interval: 1
-#subscription-userinfo: upload=29; download=12; total=10737418240000000; expire=2546249531
-#support-url: https://github.com/barry-far/V2ray-Configs
-#profile-web-page-url: https://github.com/barry-far/V2ray-Configs
-
-"""
 
 def decode_base64(encoded):
     decoded = ''
