7cf23285fe2778a935c031e0950ddfc1a0769d5d
bardiafa <143993892+barry-far@users.noreply.github.com>
2024-06-25 17:58:08 +0330

    Added New Sources 🐜

--- Files/warp.py
+++ Files/warp.py
@@ -37,7 +37,8 @@ def main():
         'https://raw.githubusercontent.com/ircfspace/warpsub/main/export/warp',
         'https://raw.githubusercontent.com/yebekhe/TVC/main/subscriptions/warp/config',
         'https://raw.githubusercontent.com/NiREvil/vless/main/hiddify/auto-gen-warp',
-        'https://raw.githubusercontent.com/hiddify/hiddify-next/main/test.configs/warp'
+        'https://raw.githubusercontent.com/hiddify/hiddify-next/main/test.configs/warp',
+        'https://raw.githubusercontent.com/mansor427/Warp-Autosub/main/subwarp/warp'
     ]
     # Process the links and filter out warp lines
     decoded_warp_lines = fetch_and_process_links(warp_links)
